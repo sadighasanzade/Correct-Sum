863b5c36aa209a3af7dc77c78dae0b7d24829477
sadighasanzade <sadiqhesensr@gmail.com>
2023-08-01 10:08:26 +0400

    Project structure is set

---
+++ app/src/main/java/com/sadig/correctsum/App.kt
@@ -0,0 +1,8 @@
+package com.sadig.correctsum
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
---
+++ app/src/main/java/com/sadig/correctsum/ui/screen/GameScreen.kt
@@ -0,0 +1,25 @@
+package com.sadig.correctsum.ui.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun gameScreen() {
+    
+}
+
+@Composable
+fun progress() {
+    
+}
+
+
+@Composable
+fun questionLayout() {
+    
+}
+
+
+@Composable
+fun answers() {
+    
+}
